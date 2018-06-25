54a861fae5d3266e54f7e7ad0c050c23b74f6945
warren <wangyunfancc@163.com>
2018-06-25 15:43:10 +0800

    瀑布流布局

--- JS进阶_6.25/js/script.js
+++ JS进阶_6.25/js/script.js
@@ -1,12 +1,50 @@
 window.onload = function () {
     // 1. 实现瀑布流布局
-    pubuliu("main", "box");
+    pu("main", "box");
+
+    // 2. 动态加载图片
+    window.onscroll = function () {
+        if (checkWillLoadImg()) {
+            // 造数据
+            let dataArr = [
+                {"src": "2.jpeg"},
+                {"src": "2.jpeg"},
+                {"src": "5.jpeg"},
+                {"src": "2.jpeg"},
+                {"src": "6.jpeg"},
+                {"src": "2.jpeg"},
+                {"src": "1.jpeg"},
+                {"src": "2.jpeg"},
+                {"src": "8.jpeg"},
+                {"src": "2.jpeg"},
+                {"src": "8.jpeg"},
+            ];
+
+            // 创建元素
+            for (let i = 0; i < dataArr.length; i++) {
+                let newBox = document.createElement("div");
+                newBox.className = "box";
+                $("main").appendChild(newBox);
+
+                let newPic = document.createElement("div");
+                newPic.className = "pic";
+                newBox.appendChild(newPic);
+
+                let newImg = document.createElement("img");
+                newImg.src = "img/" + dataArr[i].src;
+                newPic.appendChild(newImg);
+            }
+
+            // 重新布局
+            pu("main", "box");
+        }
+    }
 };
 
 /*
 * 实现瀑布流
 * */
-function pubuliu(parent, child) {
+function pu(parent, child) {
     // 1. 父盒子居中
     // 1.1 获取所有的盒子
     let allBox = $(parent).getElementsByClassName(child);
@@ -27,7 +65,7 @@ function pubuliu(parent, child) {
 
     // 2. 子盒子的定位
     // 2.1 定义高度数字
-    let height = [], boxHeight = 0, minBoxHeight = 0, minBoxIndex = 0;
+    let heightArr = [], boxHeight = 0, minBoxHeight = 0, minBoxIndex = 0;
 
     // 2.2 遍历子盒子
     for (let i = 0; i < allBox.length; i++) {
@@ -49,10 +87,29 @@ function pubuliu(parent, child) {
             allBox[i].style.left = minBoxIndex * boxWidth + 'px';
             allBox[i].style.top = minBoxHeight + 'px';
 
-            // 更新数组中的
+            // 更新数组中的高度
             heightArr[minBoxIndex] += boxHeight;
         }
     }
+}
+
+
+// 判断是否具备加载图片的条件
+function checkWillLoadImg() {
+    // 1. 获取最后一个盒子
+    let allBox = document.getElementsByClassName("box");
+    let lastBox = allBox[allBox.length - 1];
+
+    // 2. 求出最后一个盒子自身高度的一半 + offsetTop
+    let lastBoxDis = lastBox.offsetHeight * 0.5 + lastBox.offsetTop;
+
+    // 3. 求出屏幕的高度
+    let screenW = document.body.clientHeight || document.documentElement.clientHeight;
+
+    // 4. 求出页面偏离浏览器的高度
+    let scrollTop = scroll().top;
+
+    return lastBoxDis <= screenW + scrollTop;
 }
 
 function getMinBoxIndex(arr, val) {
