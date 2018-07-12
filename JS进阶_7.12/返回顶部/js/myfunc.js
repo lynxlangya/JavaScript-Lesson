88f6da773fc169ee7b14714482f7e979c48fc15b
warren <wangyunfancc@163.com>
2018-07-12 16:19:56 +0800

    返回顶部

---
+++ JS进阶_7.12/返回顶部/js/myfunc.js
@@ -0,0 +1,31 @@
+/*
+*
+* 获取滚动的头部距离和左边的距离
+*
+* */
+
+function scroll() {
+    if (window.pageYOffset !== null) {
+        return{
+            top: window.pageYOffset,
+            left: window.pageXOffset
+        }
+    }else if (document.compatMode === "CSS1Compat") {
+        return{
+            top: document.documentElement.scrollTop,
+            left: document.documentElement.scrollLeft
+        }
+    }
+
+    return{
+        top: document.body.scrollTop,
+        left: document.body.scrollLeft
+    }
+}
+
+function show(obj) {
+    return obj.style.display = 'block';
+}
+function hide(obj) {
+    return obj.style.display = '';
+}
